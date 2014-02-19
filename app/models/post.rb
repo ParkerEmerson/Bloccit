c492f49ce34e5f846996e82a1f9a4b86286ce53a
Parker <parkeremerson7@gmail.com>
2014-02-19 16:02:22 -0700

    Added Pics to Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
   belongs_to :topic
-  attr_accessible :body, :title, :topic
+  attr_accessible :body, :title, :topic, :image
 
 
   default_scope order('created_at DESC')
@@ -11,5 +11,7 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true  
+
+  mount_uploader :image, ImageUploader
 
 end
