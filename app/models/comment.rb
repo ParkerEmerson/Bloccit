a20e0b5e3d2b3fbb99a4556b6663bb0b787f7f9d
Parker <parkeremerson7@gmail.com>
2014-03-06 16:27:10 -0700

    implemented favorites

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   attr_accessible :body
+
+  after_create :send_favorite_emails
+
+  private
+
+  def send_favorite_emails
+    self.post.favorites.each do |favorite|
+      FavoriteMailer.new_comment(favorite.user, self.post, self).deliver
+    end
+  end
 end
